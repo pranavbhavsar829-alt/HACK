c30228752ea99fde168ea1b07bbc66169ce6f38d
pranavbhavsar829-alt <pranavbhavsar829@gmail.com>
2026-01-03 17:27:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,2 @@
-#!/bin/bash
-
-# Start the fetcher (bot logic) in the background
 python fetcher.py &
-
-# Start the web server (dashboard) in the foreground
 gunicorn server:app --bind 0.0.0.0:$PORT
